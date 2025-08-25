5d21cc10405a8202536a4820f6aaba6841bc5c0e
Jack Morton <jjm229@exeter.ac.uk>
2025-08-25 23:27:12 +0100

    anti email scraping

--- src/website/static/scripts/main.js
+++ src/website/static/scripts/main.js
@@ -2,15 +2,11 @@ window.addEventListener("load", function() {
 
 	"use strict";
 
-	// Make canvas elements fill their parent
-	document.querySelectorAll("canvas").forEach(canvas => {
-		const parent = canvas.parentElement
-		const resize = function() {
-			canvas.width = parent.clientWidth;
-			canvas.height = parent.clientHeight;
-		};
-		resize();
-		window.addEventListener("resize", resize);
-	});
+	// Basic email scraping prevention
+	for (const elt of document.getElementsByClassName("email")) {
+		const addr = elt.textContent.replace("(at)", "@").replaceAll("(dot)", ".");
+		elt.textContent = addr;
+		elt.href = `mailto:${addr}`;
+	}
 
 });
